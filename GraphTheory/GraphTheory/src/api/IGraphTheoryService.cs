caceadee39858c5592f708413a2a7522c663b2f6
Michel Jouaux <micheljouaux@gmx.de>
2023-05-14 18:54:24 +0200

    Add: Created IGraphTheroyService interface

---
+++ GraphTheory/GraphTheory/src/api/IGraphTheoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTheory.src.api
+{
+    internal interface IGraphTheoryService
+    {
+        public int DistanceOfRoutes(Route[] graph, string[] route);
+
+        public int NumberOfRoutes(Route[] graph, string start, string end);
+
+        public int ShortesRoute(Route[] graph, string start, string end);
+    }
+}
