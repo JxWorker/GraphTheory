59f1dff7cd01195de97f863e99bd75d0fc9f952d
Michel Jouaux <micheljouaux@gmx.de>
2023-05-14 14:17:31 +0200

    Add: Created Route class

---
+++ GraphTheory/GraphTheory/src/api/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTheory.src.api
+{
+    internal class Route
+    {
+        private string Start { get; }
+        private string End { get; }
+        private int Distance { get; }
+
+        public Route(string pStart, string pEnd, int pDistance)
+        {
+            Start = pStart;
+            End = pEnd;
+            Distance = pDistance;
+        }
+    }
+}
