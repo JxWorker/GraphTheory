2a118819dcb6bb049094e7d1b7fecc57045be8d3
Michel Jouaux <micheljouaux@gmx.de>
2023-06-09 00:35:00 +0200

    Fix: Finished ResultWindowControl.xaml/.cs

--- GraphTheory/GraphTheory/src/wpf/ResultWindowControl.xaml.cs
+++ GraphTheory/GraphTheory/src/wpf/ResultWindowControl.xaml.cs
@@ -1,21 +1,5 @@
 ﻿using GraphTheory.src.api;
-using GraphTheory.src.component;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace GraphTheory.src.wpf
 {
@@ -25,16 +9,16 @@ namespace GraphTheory.src.wpf
     public partial class ResultWindowControl : UserControl
     {
         private readonly string[] tasks = {
-            "Length of the route A-B-C:",
-            "Length of route A-D:",
-            "Length of Route A-D-C:",
-            "Length of Route A-E-B-C-D:",
-            "Length of route A-E-D:",
-            "Number of routes that start at C and also end at C and have no more than 3 stops:",
-            "Number of routes that start at A and end at C, with exactly 4 stops:",
-            "Length of the shortest route from A to C:",
-            "Length of the shortest route from B to B:",
-            "Number of routes from C to C with length < 30:"};
+            "1. Length of the route A-B-C:",
+            "2. Length of route A-D:",
+            "3. Length of Route A-D-C:",
+            "4. Length of Route A-E-B-C-D:",
+            "5. Length of route A-E-D:",
+            "6. Number of routes that start at C and also end at C and have no more than 3 stops:",
+            "7. Number of routes that start at A and end at C, with exactly 4 stops:",
+            "8. Length of the shortest route from A to C:",
+            "9. Length of the shortest route from B to B:",
+            "10. Number of routes from C to C with length < 30:"};
         private string[] results = new string[10];
 
         private readonly Route[] graph;
@@ -47,17 +31,17 @@ namespace GraphTheory.src.wpf
             this.graphTheoryService = graphTheoryService;
             this.graph = graph;
 
-            SetLabel(tasks, task_stackpanel.Children);
             CalculateResult();
-            SetLabel(results, result_stackpanel.Children);
+            ConcatResult();
+            SetLabel(results, task_stackpanel.Children);
         }
 
-        private void SetLabel(string[] text, UIElementCollection labels)
+        private void SetLabel(string[] text, UIElementCollection uIElementCollection)
         {
             int i = 0;
-            foreach (Label label in labels)
+            foreach (TextBlock element in uIElementCollection)
             {
-                label.Content = text[i++];
+                element.Text = text[i++];
             }
         }
 
@@ -68,11 +52,19 @@ namespace GraphTheory.src.wpf
             results[2] = graphTheoryService.DistanceOfRoutes(graph, new string[] { "A", "D", "C" });
             results[3] = graphTheoryService.DistanceOfRoutes(graph, new string[] { "A", "E", "B", "C", "D" });
             results[4] = graphTheoryService.DistanceOfRoutes(graph, new string[] { "A", "E", "D" });
-            results[5] = graphTheoryService.NumberOfRoutes(graph, "C", "C", 3, 4, 3, 0);
-            results[6] = graphTheoryService.NumberOfRoutes(graph, "A", "C", 2, 4, 4, 0);
+            results[5] = graphTheoryService.NumberOfRoutes(graph, "C", "C", 3, 3, 3, 0);
+            results[6] = graphTheoryService.NumberOfRoutes(graph, "A", "C", 2, 3, 4, 0);
             results[7] = graphTheoryService.ShortesRoute(graph, "A", "C");
             results[8] = graphTheoryService.ShortesRoute(graph, "B", "B");
-            results[9] = graphTheoryService.NumberOfRoutes(graph, "C", "C", 6, 10, 0, 30);
+            results[9] = graphTheoryService.NumberOfRoutes(graph, "C", "C", 6, 3, 0, 30);
+        }
+
+        private void ConcatResult()
+        {
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = tasks[i] + " " + results[i];
+            }
         }
     }
 }
