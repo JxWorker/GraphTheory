1fe97cd0bcf9974b3ea53e9dfc1dbf8370ebeee8
Michel Jouaux <micheljouaux@gmx.de>
2023-06-09 04:43:48 +0200

    Add: Added 7 methods: ComboBox_SelectionChanged, AITGraph_CalculateButtonClicked, AITGraph_TasksButtonClicked, TestGraph_CalculateButtonClicked, TestGraph_TasksButtonClicked, CreatedGraph_CalculateButtonClicked, CreatedGraph_TasksButtonClicked

--- GraphTheory/GraphTheory/MainWindow.xaml.cs
+++ GraphTheory/GraphTheory/MainWindow.xaml.cs
@@ -2,19 +2,8 @@
 using GraphTheory.src.component;
 using GraphTheory.src.wpf;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GraphTheory
 {
@@ -23,17 +12,19 @@ namespace GraphTheory
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string input = "";
+        private ExistingGraphOptionControl existingGraphOptionControl;
+        private CreateGraphControl createGraphControl;
+        private CalculateWindowControl calculateWindowControl;
+        private ResultWindowControl resultWindowControl;
+
+        private IGraphTheoryService graphTheoryService;
+
         private Route[] graphAIT = { new Route("A", "B", 5), new Route("B", "C", 4), new Route("C", "D", 8),
             new Route("D", "C", 8), new Route("D", "E", 6), new Route("A", "D", 5), new Route("C", "E", 2),
             new Route("E", "B", 3), new Route("A", "E", 7) };
         private Route[] graphTest = { new Route("A", "B", 2), new Route("B", "C", 1), new Route("B", "E", 3),
             new Route("C", "G", 3), new Route("G", "F", 7), new Route("F", "G", 7), new Route("E", "F", 4),
             new Route("E", "H", 9), new Route("E", "D", 7), new Route("D", "A", 5), new Route("H", "F", 6) };
-        private Route[] graphCreated;
-        private Route[] graph;
-
-        private IGraphTheoryService graphTheoryService;
 
         public MainWindow()
         {
@@ -42,13 +33,84 @@ namespace GraphTheory
             graphTheoryService = new Algorithm();
         }
 
-        void testClick(object sender, EventArgs e)
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            CalculateWindowControl test = new CalculateWindowControl(graphTheoryService, graphTest);
-            ResultWindowControl test1 = new ResultWindowControl(graphTheoryService, graphAIT);
+            selectContainer_grid.Children.Clear();
+            windowContainer_grid.Children.Clear();
+            ComboBoxItem comboBoxItem = (ComboBoxItem)graph_combobox.SelectedItem;;
 
-            windowContainer.Children.Add(test1);
-            test1.Visibility = Visibility.Visible;
+            switch(comboBoxItem.Content.ToString())
+            {
+                case "Create graph":
+                    createGraphControl = new CreateGraphControl();
+                    createGraphControl.CalculateButtonClicked += CreatedGraph_CalculateButtonClicked;
+                    createGraphControl.TasksButtonClicked += CreatedGraph_TasksButtonClicked;
+                    selectContainer_grid.Children.Add(createGraphControl);
+                    createGraphControl.Visibility = Visibility.Visible;
+                    break;
+                case "AIT gragh":
+                    existingGraphOptionControl = new ExistingGraphOptionControl();
+                    existingGraphOptionControl.CalculateButtonClicked += AITGraph_CalculateButtonClicked;
+                    existingGraphOptionControl.TasksButtonClicked += AITGraph_TasksButtonClicked;
+                    selectContainer_grid.Children.Add(existingGraphOptionControl);
+                    existingGraphOptionControl.Visibility = Visibility.Visible;
+                    break;
+                case "Test graph":
+                    existingGraphOptionControl = new ExistingGraphOptionControl();
+                    existingGraphOptionControl.CalculateButtonClicked += TestGraph_CalculateButtonClicked;
+                    existingGraphOptionControl.TasksButtonClicked += TestGraph_TasksButtonClicked;
+                    selectContainer_grid.Children.Add(existingGraphOptionControl);
+                    existingGraphOptionControl.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void AITGraph_CalculateButtonClicked(object sender, EventArgs e)
+        {
+            windowContainer_grid.Children.Clear();
+            calculateWindowControl = new CalculateWindowControl(graphTheoryService, graphAIT);
+            windowContainer_grid.Children.Add(calculateWindowControl);
+            calculateWindowControl.Visibility = Visibility.Visible;
+        }
+
+        private void AITGraph_TasksButtonClicked(object sender, EventArgs e)
+        {
+            windowContainer_grid.Children.Clear();
+            resultWindowControl = new ResultWindowControl(graphTheoryService, graphAIT);
+            windowContainer_grid.Children.Add(resultWindowControl);
+            resultWindowControl.Visibility = Visibility.Visible;
+        }
+
+        private void TestGraph_CalculateButtonClicked(object sender, EventArgs e)
+        {
+            windowContainer_grid.Children.Clear();
+            calculateWindowControl = new CalculateWindowControl(graphTheoryService, graphTest);
+            windowContainer_grid.Children.Add(calculateWindowControl);
+            calculateWindowControl.Visibility = Visibility.Visible;
+        }
+
+        private void TestGraph_TasksButtonClicked(object sender, EventArgs e)
+        {
+            windowContainer_grid.Children.Clear();
+            resultWindowControl = new ResultWindowControl(graphTheoryService, graphTest);
+            windowContainer_grid.Children.Add(resultWindowControl);
+            resultWindowControl.Visibility = Visibility.Visible;
+        }
+
+        private void CreatedGraph_CalculateButtonClicked(object sender, Route[] graph)
+        {
+            windowContainer_grid.Children.Clear();
+            calculateWindowControl = new CalculateWindowControl(graphTheoryService, graph);
+            windowContainer_grid.Children.Add(calculateWindowControl);
+            calculateWindowControl.Visibility = Visibility.Visible;
+        }
+
+        private void CreatedGraph_TasksButtonClicked(object sender, Route[] graph)
+        {
+            windowContainer_grid.Children.Clear();
+            resultWindowControl = new ResultWindowControl(graphTheoryService, graph);
+            windowContainer_grid.Children.Add(resultWindowControl);
+            resultWindowControl.Visibility = Visibility.Visible;
         }
     }
 }
